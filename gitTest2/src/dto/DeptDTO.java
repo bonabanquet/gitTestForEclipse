bbb1fcb5bad13b56354d9812b018738dd79f2917
SY <mynjy94@gmail.com>
2021-05-26 09:44:01 +0900

    add DeptDTO.class

---
+++ gitTest2/src/dto/DeptDTO.java
@@ -0,0 +1,15 @@
+package dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class DeptDTO {
+	int deptno;
+	String dname;
+	String loc;
+	
+}
